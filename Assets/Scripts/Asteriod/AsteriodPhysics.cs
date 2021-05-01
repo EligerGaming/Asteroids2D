6f0e91933e275e2f9fc805986094fb1a3d89efa4
EligerGaming <eligergaming@gmail.com>
2021-04-30 20:33:02 -0500

    New Score Counter & Bug Fixes
    -Added a score counter that keeps track of every time you destroy an asteroid with your blaster.
    
    -Added tags for the plasma projectiles and the player ship to keep track of specific collisions.
    
    -Fixed a bug where asteroids would destroy each other if they spawned on top of each other

--- Assets/Scripts/Asteriod/AsteriodPhysics.cs
+++ Assets/Scripts/Asteriod/AsteriodPhysics.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class AsteriodPhysics : MonoBehaviour
 {
+    public GameObject currentScore;
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
-        Destroy(gameObject);
+        if (collider2D.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+        
+        if (collider2D.tag == "playerProjectile")
+        {
+            Destroy(gameObject);
+            currentScore.GetComponent<Score>().currentScore += 1;
+        }
+
     } 
     [SerializeField] float asteriodSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
+        currentScore = GameObject.Find("currentScore");
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, asteriodSpeed * -1);
     }
 
